a4fe79be323b8ccb1aa3e1687e7f8d64999a39ea
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-13 20:17:13 +0100

    Creamos un componente por cada página de la web

---
+++ src/app/pages/trabajos/trabajos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajos',
+  imports: [],
+  templateUrl: './trabajos.component.html',
+  styleUrl: './trabajos.component.css'
+})
+export class TrabajosComponent {
+
+}
