ce912ca4aa44dec8cc9413fc6d875d005d1c5a9b
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-15 18:27:59 +0100

    Error404
    Nunca empezamos un componente por un número

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { TrabajosComponent } from './pages/trabajos/trabajos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { VistaTrabajoComponent } from './pages/vista-trabajo/vista-trabajo.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "home" },
@@ -10,7 +11,8 @@ export const routes: Routes = [
     { path: "servicios", component: TrabajosComponent },
     { path: "servicios/:url", component: VistaTrabajoComponent},
     { path: "contacto", component: ContactoComponent},
-    { path: "**", redirectTo: "home" }
+    { path: "**", component: Error404Component}
+    // { path: "**", redirectTo: "home" }
 ];
 
 
