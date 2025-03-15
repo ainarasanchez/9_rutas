bff2c4ec9150575324befa197c50c69bcd2df08c
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-15 18:54:06 +0100

    El activatedRoute recoge la parte dinámica de la ruta
    :ulr/<macrame> (capturamos la ruta)

--- src/app/pages/vista-trabajo/vista-trabajo.component.ts
+++ src/app/pages/vista-trabajo/vista-trabajo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vista-trabajo',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './vista-trabajo.component.css'
 })
 export class VistaTrabajoComponent {
+  // necesito saber cual es el dato a  cargar, y ese dato lo tengo en la url de mi nevagador. 
+
+  // Opción 1. ActivatedRoute => inyectable de libreria de datos
+  activatedRoute = Inject(ActivatedRoute);
+
+  ngOnInit() {
+  // le voy a preguntar al activatedRoute mis parámetros dinámicos de ruta 
+  this.activatedRoute.params.subscribe((params: any) => {
+    console.log(params);
+  
+  })
+
+  }
+
 
 }
